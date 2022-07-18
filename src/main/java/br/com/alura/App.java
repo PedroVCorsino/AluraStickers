92a97e0f7efae94aa9109963255a934433b7128f
Pedro Vinicius <pedro.santos@metasix.com.br>
2022-07-18 19:00:40 -0300

    Parseando body com Gson

--- src/main/java/br/com/alura/App.java
+++ src/main/java/br/com/alura/App.java
@@ -1,20 +1,35 @@
 package br.com.alura;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import br.com.alura.model.entity.Filme;
+import br.com.alura.model.entity.pojo.Filmes;
+
+
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args ) throws IOException, InterruptedException
-    {
+public class App {
+    public static void main( String[] args ) throws IOException, InterruptedException, Exception{
+
+        ObjectMapper mapper = new ObjectMapper();
+
         String url = "https://api.mocki.io/v2/549a5d8b";
         URI uri = URI.create(url);
         HttpClient client = HttpClient.newHttpClient();
@@ -22,6 +37,18 @@ public class App
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
 
-        System.out.println(body);
+       
+        Filmes filmes = new Gson().fromJson(body, Filmes.class);
+
+        for (Filme filme : filmes.getFilmes()) {
+            System.out.println("Classificação: " + filme.getImDbRating());
+            System.out.println("Titulo: " + filme.getTitle());
+            System.out.println("Imagem: "+ filme.getImage());
+            System.out.println();
+        }    
+
+        
     }
+
+
 }

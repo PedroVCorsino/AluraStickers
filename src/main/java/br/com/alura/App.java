82edebc3b0906a030f768b69b558fbf937295cd0
Pedro Vinicius <pedro.santos@metasix.com.br>
2022-07-18 15:11:41 -0300

    Fazendo o basico para consumir uma api

--- src/main/java/br/com/alura/App.java
+++ src/main/java/br/com/alura/App.java
@@ -1,13 +1,27 @@
 package br.com.alura;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException, InterruptedException
     {
-        System.out.println( "Hello World!" );
+        String url = "https://api.mocki.io/v2/549a5d8b";
+        URI uri = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(uri).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+
+        System.out.println(body);
     }
 }
